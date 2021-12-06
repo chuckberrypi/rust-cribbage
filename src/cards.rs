23f40569f14382a44eb652d9c549f72df0278ebb
David Stearns <d.f.stearns@gmail.com>
2021-12-05 19:02:39 -0800

    itertools used

--- src/cards.rs
+++ src/cards.rs
@@ -1,6 +1,9 @@
 use enum_iterator::IntoEnumIterator;
+use itertools::Itertools;
+use rand::{thread_rng, Rng};
+use std::fmt;
 
-#[derive(Debug, IntoEnumIterator, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy, IntoEnumIterator)]
 pub enum Suit {
     Hearts,
     Spades,
@@ -8,7 +11,7 @@ pub enum Suit {
     Clubs,
 }
 
-#[derive(Debug, IntoEnumIterator, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy, IntoEnumIterator)]
 pub enum Value {
     Ace,
     Two,
@@ -25,19 +28,168 @@ pub enum Value {
     King,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Card {
     pub val: Value,
     pub suit: Suit,
 }
 
-impl Suit {
-    pub fn to_char(&self) -> char {
-        match self {
-            Suit::Hearts => '\u{2665}',
-            Suit::Spades => '\u{2660}',
-            Suit::Diamonds => '\u{2666}',
-            Suit::Clubs => '\u{2663}',
+// impl fmt::Debug for Card {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{}", self)
+//     }
+// }
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Value::Ace => "A",
+                Value::Two => "2",
+                Value::Three => "3",
+                Value::Four => "4",
+                Value::Five => "5",
+                Value::Six => "6",
+                Value::Seven => "7",
+                Value::Eight => "8",
+                Value::Nine => "9",
+                Value::Ten => "T",
+                Value::Jack => "J",
+                Value::Queen => "Q",
+                Value::King => "K",
+            }
+        )
+    }
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Suit::Hearts => '\u{2665}',
+                Suit::Spades => '\u{2660}',
+                Suit::Diamonds => '\u{2666}',
+                Suit::Clubs => '\u{2663}',
+            }
+        )
+    }
+}
+
+pub struct Deck(pub Vec<Card>);
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.val, self.suit)
+    }
+}
+
+fn rand_swap_remove<T>(v: &mut Vec<T>, r: &mut rand::rngs::ThreadRng) -> Option<T> {
+    if v.len() == 0 {
+        return None;
+    }
+    let i = r.gen_range(0..v.len());
+    Some(v.swap_remove(i))
+}
+
+// impl From<&Vec<&Card>> for Deck {
+//     fn from(v: [Card]) -> Self {
+//         let cards: Vec<Card> = Vec::new();
+//         Deck(cards.clone_from_slice(v))
+//     }
+// }
+
+impl From<&[Card]> for Deck {
+    fn from(c: &[Card]) -> Self {
+        let mut v: Vec<Card> = Vec::new();
+        v.clone_from_slice(c);
+        Deck(v)
+    }
+}
+
+impl From<&Vec<&Card>> for Deck {
+    fn from(v: &Vec<&Card>) -> Self {
+        let mut cards: Vec<Card> = Vec::new();
+        for card in v.iter() {
+            cards.push((**card).clone());
         }
+        Deck(cards)
+    }
+}
+
+impl Deck {
+    // pub fn from(c: &Vec<&Card>) -> Self {
+    // pub fn from(c: &[&Card]) -> Self {
+    //     let mut cards: Vec<Card> = Vec::new();
+    //     for card in c.iter() {
+    //         cards.push((**card).clone());
+    //     }
+    //     Deck(cards)
+    // }
+
+    pub fn new() -> Self {
+        let mut d: Vec<Card> = Vec::with_capacity(52);
+        for s in Suit::into_enum_iter() {
+            for v in Value::into_enum_iter() {
+                d.push(Card { val: v, suit: s });
+            }
+        }
+        Deck(d)
+    }
+
+    pub fn new_shuffled() -> Self {
+        let mut rng = thread_rng();
+        let mut d: Vec<Card> = Vec::with_capacity(52);
+        for _ in 0..52 {
+            d.push(Card {
+                val: Value::Ace,
+                suit: Suit::Hearts,
+            });
+        }
+        let mut positions: Vec<usize> = Vec::with_capacity(52);
+        for i in 0..52 as usize {
+            positions.push(i);
+        }
+        // println!("Positions: {:?}", positions);
+        for s in Suit::into_enum_iter() {
+            for v in Value::into_enum_iter() {
+                if let Some(i) = rand_swap_remove(&mut positions, &mut rng) {
+                    d[i] = Card { suit: s, val: v };
+                }
+            }
+        }
+        Deck(d)
+    }
+
+    pub fn shuffle(self) -> Self {
+        let mut rng = thread_rng();
+        let mut positions: Vec<usize> = Vec::with_capacity(self.0.len());
+        for i in 0..self.0.len() {
+            positions.push(i);
+        }
+        let mut d: Vec<Card> = Vec::with_capacity(self.0.len());
+        while let Some(i) = rand_swap_remove(&mut positions, &mut rng) {
+            d.push(self.0[i].clone());
+        }
+        Self(d)
+    }
+
+    pub fn swap_remove_rand(&mut self, r: &mut rand::rngs::ThreadRng) -> Card {
+        self.0.swap_remove(r.gen_range(0..self.0.len()))
+    }
+
+    pub fn deal_rand_to(&mut self, recipient: &mut Deck, r: &mut rand::rngs::ThreadRng) {
+        recipient.0.push(self.swap_remove_rand(r));
+    }
+
+    pub fn combos(&self, n: usize) -> Vec<Deck> {
+        self.0
+            .iter()
+            .combinations(n)
+            .map(|c| Deck::from(&c))
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 mod cards;
 
-use cards::{Card, Value, Suit};
+use cards::{Card, Deck};
 
 fn main() {
-    let c: Card = Card {
-        val: Value::Ace,
-        suit: Suit::Hearts,
-    };
+    let d = Deck::new();
+    let d = d.shuffle();
+    // for c in d.0.iter() {
+    //     println!("{}", c);
+    // }
 
-    println!("Hello, world! {:?}", c);
+    // println!("===================================");
+
+    let mut d2 = Deck::new_shuffled();
+    // for c in d2.0.iter() {
+    //     println!("{}", c);
+    // }
+
+    let mut rng = rand::thread_rng();
+    println!("Hello, world! {}", d2.swap_remove_rand(&mut rng));
+    // let cards: Vec<&Card> = d.0.iter().collect();
+    // let d2 = Deck::from(&&d.0[0..4]);
+    let d2 = Deck::from(&d.0[0..4]);
+    // for c in d2.combos(3).iter() {
+    //     println!("{:?}", c.0);
+    // }
+    // println!("{:?}", d2.0);
 }
